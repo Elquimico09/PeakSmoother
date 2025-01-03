2c43de8cc82eabafd5b8e8341ae5f6ab9bda6767
Vishal Sandilya <vishal.sandilya@ttu.edu>
2025-01-03 15:16:07 -0600

    tests for eic and tic working, GPR taking too long to run. Needs a fix.

--- scripts/GPR.py
+++ scripts/GPR.py
@@ -13,12 +13,15 @@ def GPR(X, y):
     """
     X = X.reshape(-1, 1)
 
-    kernel = ConstantKernel(1, (1e-2, 1e2)) * RBF(1, (1e-2, 1e2)) + WhiteKernel(1e-5, (1e-3, 1e1))
-    gpr = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10, alpha = 0)
+    length_scale = (X.max() - X.min()) / 10
+    kernel = (ConstantKernel(1, (1e-2, 1e2)) *
+    RBF(length_scale, (1e-2, 1e2)) +
+    WhiteKernel(1e-5, (1e-3, 1e1)))
+    gpr = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10, alpha = 1e-10)
 
     gpr.fit(X, y)
 
-    X_pred = np.linspace(X.min(), X.max(), 1000).reshape(-1, 1)
+    X_pred = np.linspace(X.min() - 0.1 * (X.max() - X.min()), X.max() + 0.1 * (X.max() - X.min()), 1000).reshape(-1, 1)
     y_pred, sigma = gpr.predict(X_pred, return_std=True)
 
     return X_pred, y_pred, sigma
